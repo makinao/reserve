ed929ddbdf42a7db3513a54f459a42b67c4f15ba
makinao <makinao.jp@gmail.com>
2023-11-25 21:41:20 +0900

    devise実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :users do
+    resources :profiles, only: [:edit, :update, :index]
+    get 'accounts/index'
+   end
+  resources :users
+  get 'users/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
